3f8b049751200a9784328c14e4f0bc309fc124cd
Nicholas Mutale <nicomutale@gmail.com>
2022-09-09 11:08:24 +0200

    Add Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+        <div className="flex justify-between item-center w-full h-full px-2 2xl:px-16">
+            <Image src="" alt="" />
+            </div>
+
+    </div>
+  )
+}
+
+export default Navbar
