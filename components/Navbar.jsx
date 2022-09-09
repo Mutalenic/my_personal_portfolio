a8a1c265b1d27950f5a188f70dcd6157e9eb4bb1
Nicholas Mutale <nicomutale@gmail.com>
2022-09-09 11:48:16 +0200

    Configure logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
         <div className="flex justify-between item-center w-full h-full px-2 2xl:px-16">
-            <Image src="" alt="" />
+            <Image src="/../public/assets/logo.png" alt="Logo" width="125" height="50" />
             </div>
 
     </div>
