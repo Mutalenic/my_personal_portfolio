e0af555cd8b8f75d97c748c3d99c973851642e5e
Nicholas Mutale <nicomutale@gmail.com>
2022-09-10 22:28:25 +0200

    Add grid container for images

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,26 @@
+import Image from 'next/image';
 import React from 'react'
 
 function Skills() {
   return (
     <div className="w-full lg:h-screen p-2">
         <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-            <p>Skills</p>
-            <h2>What I Can Do</h2>
+            <p className="text-xl tracking-widest uppercase text-[#1e1a95]">Skills</p>
+            <h2 className="py-4">What I Can Do</h2>
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                    <div className="grid-cols-2 gap-4 justify-center items-center">
+                        <div className="m-auto">
+                            <Image src="/../public/assets/html.png" width="64px" height="64px" alt="html icon" />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
-  )
-}
+  );
+};
 
 export default Skills;
