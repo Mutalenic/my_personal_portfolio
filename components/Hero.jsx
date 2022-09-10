eed602c6d22b61483005512f68d88214bbf8f6df
Nicholas Mutale <nicomutale@gmail.com>
2022-09-10 22:01:10 +0200

    Add skills component and import it in index js

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,34 +7,31 @@ import { CgMail } from "react-icons/cg";
 
 function Hero() {
   return (
-    <div className="w-full h-screen text-center flex">
-      <div className="max-w-[1240px] w-full h-ful mx-auto p-2 flex justify-center items-center">
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <h1 className=" py-4 tex-sm tracking-widest text-gray-600">
-            Hi, my name is
-            <span className="text-[#1e1a95]"> Nicholas Mutale</span>
+          <h1 className='py-4 text-gray-700'>
+            Hi, I&#39;m <span className='text-[#1e1a95]'>Nicholas</span>
           </h1>
-          <h2 className="py-2">I &#39;m a Full Stack Web Developer</h2>
-          <p className="py-4 max-w-[70%] m-auto">
-            I &#39;m passionate about designing and developing powerful modern web
-            appications.
+          <h1 className='py-2 text-gray-700'>a Full Stack Web Developer</h1>
+          <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
+            I&#39;m focused on building responsive front-end web applications while
+            learning back-end technologies.
           </p>
-
-          <div className=" flex items-center justify-center gap-8 max-w[330px] m-auto py-10">
-            <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale:100 ease-in duration-300">
-              <FaLinkedinIn />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale:100 ease-in duration-300">
-              <FaGithub />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale:100 ease-in duration-300">
-              <CgMail />
-            </div>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaLinkedinIn />
+              </div>
+            <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaGithub />
+              </div>
             
-           
-            <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale:100 ease-in duration-300">
-              <BsFillPersonLinesFill />
-            </div>
+              <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <CgMail />
+              </div>
+            <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <BsFillPersonLinesFill />
+              </div>
           </div>
         </div>
       </div>
