e870a13fb5a4502dd1a2c4199c97a6cf411f1e82
Nicholas Mutale <nicomutale@gmail.com>
2022-09-11 11:32:03 +0200

    Add project image

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import airquality from "../public/assets/projects/airquality.png"
 
 function Work() {
-  return (
-    <div>Work</div>
-  )
+  return <div className="w-full">
+    <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[##1e1a95]">
+            Projects
+        </p>
+        <h2>What I&#39;v Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+            <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+               <Image src={airquality} alt="Airquality image"/>
+               
+            </div>
+        </div>
+    </div>
+
+  </div>;
 }
 
-export default Work
+export default Work;
