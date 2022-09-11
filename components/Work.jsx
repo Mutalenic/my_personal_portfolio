db0d8de64d49a761111e1298fcfb62343bef8ae0
Nicholas Mutale <nicomutale@gmail.com>
2022-09-11 13:20:00 +0200

    Create workitem component and import image in workitem component

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import airquality from "../public/assets/projects/airquality.png"
+import WorkItem from "./WorkItem";
 
 function Work() {
   return <div className="w-full">
@@ -11,18 +11,7 @@ function Work() {
         </p>
         <h2>What I&#39;v Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-            <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-               <Image className="rounded-xl group-hover:opacity-10" src={airquality} alt="Airquality image"/>
-               <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                <h3 className="text-2xl text-white tracking-widest text-center">
-                    Airquality
-                </h3>
-                <p className="pb-4 pt-2 text-white text-center">React js</p>
-                <Link href="/">
-                    <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer"></p>
-                </Link>
-                </div> 
-            </div>
+            <WorkItem/>
         </div>
     </div>
 
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 import Skills from '../components/Skills'
 import Work from '../components/Work'
+import WorkItem from '../components/WorkItem'
 
 export default function Home() {
   return (
