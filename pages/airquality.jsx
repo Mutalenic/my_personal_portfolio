ae52a72c36e51a900301bd37c20947d42f1fd4cb
Nicholas Mutale <nicomutale@gmail.com>
2022-09-11 22:37:12 +0200

    Update tecnoligies list in airquality page

--- pages/airquality.jsx
+++ pages/airquality.jsx
@@ -39,6 +39,15 @@ function airquality() {
                     <p className="text-gray-600 py-2 flex items-center">
                         <RiRadioButtonFill className="p-1"/> React
                     </p>
+                    <p className="text-gray-600 py-2 flex items-center">
+                        <RiRadioButtonFill className="p-1"/> JavaScript
+                    </p>
+                    <p className="text-gray-600 py-2 flex items-center">
+                        <RiRadioButtonFill className="p-1"/> CSS
+                    </p>
+                    <p className="text-gray-600 py-2 flex items-center">
+                        <RiRadioButtonFill className="p-1"/> Redux
+                    </p>
                 </div>
             </div>
 
