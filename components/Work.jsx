79d3ae5dc8b54dd6a55adc5425c5b139c48d95a6
Nicholas Mutale <nicomutale@gmail.com>
2022-09-11 13:32:25 +0200

    Add more project images

--- components/Work.jsx
+++ components/Work.jsx
@@ -2,20 +2,44 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import WorkItem from "./WorkItem";
+import moviespace from "../public/assets/projects/moviespace2.png";
+import spcetravellers from "../public/assets/projects/SpaceTravellerHub.png";
+import digiBudgetApp from "../public/assets/projects/DigiApp.png"
+import airquality from "../public/assets/projects/airquality.png";
 
 function Work() {
-  return <div className="w-full">
-    <div className="max-w-[1240px] mx-auto px-2 py-16">
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[##1e1a95]">
-            Projects
+          Projects
         </p>
         <h2>What I&#39;v Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-            <WorkItem/>
+          <WorkItem
+            title="Airquality"
+            backgroundImg={airquality}
+            projectUrl="/"
+          />
+           <WorkItem
+            title="Airquality"
+            backgroundImg={digiBudgetApp}
+            projectUrl="/"
+          />
+           <WorkItem
+            title="Airquality"
+            backgroundImg={moviespace}
+            projectUrl="/"
+          />
+           <WorkItem
+            title="Airquality"
+            backgroundImg={spcetravellers}
+            projectUrl="/"
+          />
         </div>
+      </div>
     </div>
-
-  </div>;
+  );
 }
 
 export default Work;
