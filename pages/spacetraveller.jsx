c40c87fd00fc9f5f568780ae78b9873aebde302b
Nicholas Mutale <nicomutale@gmail.com>
2022-09-12 11:00:40 +0200

    Add image to spacetraveller page

--- pages/spacetraveller.jsx
+++ pages/spacetraveller.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { RiRadioButtonFill } from "react-icons/ri";
-import moviespaceImg from "../public/assets/projects/moviespace2.png";
+import spacetravellerImg from "../public/assets/projects/SpaceTravellerHub.png";
 
 function spacetraveller() {
   return (
@@ -13,12 +13,12 @@ function spacetraveller() {
           className="absolute z-1"
           layout="fill"
           objectFit="cover"
-          src={moviespaceImg}
-          alt="moviesspace image"
+          src={spacetravellerImg}
+          alt="SpaceTravellerHub image"
         />
         <div className=" text-white z-10 p-2 absolute top-[70%] max-w-[1240px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <h2 className="py-2">Moviespace App</h2>
-          <h3>React / Redux / CSS</h3>
+          <h2 className="py-2">Spacetreveler&#39;s Hub</h2>
+          <h3>ReactJs / Redux / CSS</h3>
         </div>
       </div>
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
@@ -26,7 +26,10 @@ function spacetraveller() {
           <p>Project</p>
           <h3>Overview</h3>
           <p>
-          Real live data from the SpaceX API. A web application built with React and Redux for a company that provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions.
+            Real live data from the SpaceX API. A web application built with
+            React and Redux for a company that provides commercial and
+            scientific space travel services. The application allows users to
+            book rockets and join selected space missions.
           </p>
           <a href="https://6262b54ee8f3d52d5200c258--tiny-bavarois-1b6015.netlify.app/">
             <button className="px-8 py-2 mt-4 mr-8">Demo</button>
